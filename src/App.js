29d4852b7b5f826510280814ab6860cbae83e566
Anne <annaconda1984@yahoo.co.uk>
2025-01-08 14:21:14 +0000

    lazy load included

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import HomePage from './components/HomePage/HomePage';
-import Contact from './components/Contact/Contact';
-import About from './components/About/About';
-import Skills from "./components/Skills/Skills";
-import Projects from "./components/Projects/Projects";
+import Loader from './components/Loader/Loader'; // Import the Loader component
 import "devicon/devicon.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+// Lazy load components
+const NavBar = React.lazy(() => import('./components/NavBar/NavBar'));
+const HomePage = React.lazy(() => import('./components/HomePage/HomePage'));
+const Contact = React.lazy(() => import('./components/Contact/Contact'));
+const About = React.lazy(() => import('./components/About/About'));
+const Skills = React.lazy(() => import('./components/Skills/Skills'));
+const Projects = React.lazy(() => import('./components/Projects/Projects'));
+
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <section id="home">
-        <HomePage />
-      </section>
-      <div className="section-divider"></div>
+      {/* Use Loader as fallback */}
+      <Suspense fallback={<Loader />}>
+        <NavBar />
+        <section id="home">
+          <HomePage />
+        </section>
+        <div className="section-divider"></div>
 
-      <section id="about">
-        <About />
-      </section>
-      <div className="section-divider"></div>
+        <section id="about">
+          <About />
+        </section>
+        <div className="section-divider"></div>
 
-      <section id="skills">
-        <Skills />
-      </section>
-      <div className="section-divider"></div>
+        <section id="skills">
+          <Skills />
+        </section>
+        <div className="section-divider"></div>
 
-      <section id="projects">
-        <Projects />
-      </section>
-      <div className="section-divider"></div>
+        <section id="projects">
+          <Projects />
+        </section>
+        <div className="section-divider"></div>
 
-      <section id="contact">
-        <Contact />
-      </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </Suspense>
     </div>
   );
 }
